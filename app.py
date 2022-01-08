7e277c6eaf3cc912f83532d5244dcf7d5a685aea
OPelham <oliverpelham@gmail.com>
2022-01-08 15:00:12 +1300

    refactoring to clean up code

--- app.py
+++ app.py
@@ -10,58 +10,39 @@ def index():
 
 @app.route('/news/')
 def news():
-    articles = call_news()
-    return render_template('news.html', articles=articles)
-
-
-def call_news():
-    custom_headers = {"x-api-key": "59bceb20876d4a8f97bb58762a79b86a"}
-    news_request = requests.get("https://newsapi.org/v2/everything?domains=rnz.co.nz,cnn.com,bbc.com,dw.com&pagesize=100&language=en", headers=custom_headers)
-    news_response_json = dict(news_request.json())
-    articles = news_response_json.get("articles")
-    return articles
+    end_point = "https://newsapi.org/v2/everything?domains=rnz.co.nz,cnn.com,bbc.com,dw.com&pagesize=100&language=en"
+    articles = call_news_api(end_point)
+    return render_template('news.html', articles=articles, title='news')
 
 
 @app.route('/technology/')
 def technology():
-    tech_articles = call_technology()
-    return render_template('news.html', articles=tech_articles)
-
-
-def call_technology():
-    custom_headers = {"x-api-key": "59bceb20876d4a8f97bb58762a79b86a"}
-    news_request = requests.get("https://newsapi.org/v2/top-headlines?category=technology&pagesize=100&language=en", headers=custom_headers)
-    news_response_json = dict(news_request.json())
-    articles = news_response_json.get("articles")
-    return articles
+    end_point = "https://newsapi.org/v2/top-headlines?category=technology&pagesize=100&language=en"
+    tech_articles = call_news_api(end_point)
+    return render_template('news.html', articles=tech_articles, title='technology')
 
 
 @app.route('/business/')
 def business():
-    business_articles = call_business()
-    return render_template('news.html', articles=business_articles)
-
-
-def call_business():
-    custom_headers = {"x-api-key": "59bceb20876d4a8f97bb58762a79b86a"}
-    business_request = requests.get("https://newsapi.org/v2/top-headlines?category=business&pagesize=100&language=en", headers=custom_headers)
-    news_response_json = dict(business_request.json())
-    articles = news_response_json.get("articles")
-    return articles
+    end_point = "https://newsapi.org/v2/top-headlines?category=business&pagesize=100&language=en"
+    business_articles = call_news_api(end_point)
+    return render_template('news.html', articles=business_articles, title='business')
 
 
 @app.route('/sport/')
 def sport():
-    sport_articles = call_sport()
-    return render_template('news.html', articles=sport_articles)
+    end_point = "https://newsapi.org/v2/top-headlines?category=sport&pagesize=100&language=en"
+    sport_articles = call_news_api(end_point)
+    return render_template('news.html', articles=sport_articles, title='sport')
 
 
-def call_sport():
+def call_news_api(end_point):
     custom_headers = {"x-api-key": "59bceb20876d4a8f97bb58762a79b86a"}
-    sport_request = requests.get("https://newsapi.org/v2/top-headlines?category=sport&pagesize=100&language=en", headers=custom_headers)
-    news_response_json = dict(sport_request.json())
+    news_request = requests.get(end_point, headers=custom_headers)
+    news_response_json = dict(news_request.json())
     articles = news_response_json.get("articles")
     return articles
+
 
 if __name__ == "__main__":
     app.run(debug=True)
