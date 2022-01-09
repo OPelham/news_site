5bc2bd3560a936d43a0407d2b4fef9fd0a460343
OPelham <oliverpelham@gmail.com>
2022-01-09 15:51:28 +1300

    reformatting navbar

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, redirect
 import requests
+from pytz import timezone
+from datetime import datetime
 import os
 # import hidden_space
 
-
+# use hidden space when testing on local and environment variables when using heroku
 # WEATHER_API_KEY = hidden_space.WEATHER_API_KEY
 # NEWS_API_KEY = hidden_space.NEWS_API_KEY
 WEATHER_API_KEY = os.environ.get("WEATHER_KEY")
 NEWS_API_KEY = os.environ.get("NEWS_KEY")
-# WEATHER_API_KEY = requests.get("https://api.heroku.com/apps/pelicannews/config-vars", "Accept: application/vnd.heroku+json; version=3").get("WEATHER_KEY")
-# NEWS_API_KEY = requests.get("https://api.heroku.com/apps/pelicannews/config-vars", "Accept: application/vnd.heroku+json; version=3").get("NEWS_KEY")
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -52,6 +53,11 @@ def call_news_api(end_point):
     news_request = requests.get(end_point, headers=custom_headers)
     news_response_json = dict(news_request.json())
     articles = news_response_json.get("articles")
+
+    # datetime_format = "%d-%m-%Y | %H:%M:%S %Z%z"
+    # for article in articles:
+    #     old_time_format = article.get("publishedAt")
+    #     nz_time_format = old_time_format.astimezone(timezone('Pacific/Auckland')).strftime(datetime_format)
     return articles
 
 
@@ -109,6 +115,21 @@ def enquire_7_day_forcast():
     _end_point_base = "https://api.openweathermap.org/data/2.5/weather?q={}, NZ&appid={}&units=metric"
     end_point = _end_point_base.format(location_default, _API_key)
     weather_response = requests.get(end_point).json()
+
+
+@app.route('/football/')
+def football():
+    return render_template('football.html')
+
+
+@app.route('/covid/')
+def covid():
+    return render_template('covid.html')
+
+
+@app.route('/quiz/')
+def quiz():
+    return render_template('quiz.html')
 
 
 if __name__ == "__main__":
